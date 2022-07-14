12a0f6d57c5124747a9ae33daae6d5a589fce9e0
jatinggg <jatin.js.shrivastava@gmail.com>
2022-07-14 15:38:08 +0530

    project commintted

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace consoleapp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is ConsoleApp 4");
+        }
+    }
+}
